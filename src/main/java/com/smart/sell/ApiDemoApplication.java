37c39c683bb01d1d1cc40c63999f3b6e5a6eabb9
Srivastava <brijesh.c.kumar@capgemini.com>
2019-08-15 21:21:52 +0530

    Added SpringInitializer For War.

--- src/main/java/com/smart/sell/ApiDemoApplication.java
+++ src/main/java/com/smart/sell/ApiDemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.smart.sell"})
-public class ApiDemoApplication {
+public class ApiDemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiDemoApplication.class, args);
