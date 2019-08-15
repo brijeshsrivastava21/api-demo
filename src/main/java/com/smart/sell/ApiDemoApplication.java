f4b9ed06976768067d782fb39feb8e936633b5ee
brijeshsrivastava21 <brijesh.c.kumar@capgemini.com>
2019-08-15 21:26:29 +0530

    Added Import

--- src/main/java/com/smart/sell/ApiDemoApplication.java
+++ src/main/java/com/smart/sell/ApiDemoApplication.java
@@ -6,6 +6,7 @@ package com.smart.sell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
