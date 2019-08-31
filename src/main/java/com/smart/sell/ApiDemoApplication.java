0202dc4d787bc3c673adf03242f5d4ffca2ad9c4
Srivastava <brijesh.c.kumar@capgemini.com>
2019-08-31 16:09:29 +0530

    Chnages

--- src/main/java/com/smart/sell/ApiDemoApplication.java
+++ src/main/java/com/smart/sell/ApiDemoApplication.java
@@ -5,12 +5,14 @@ package com.smart.sell;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.smart.sell"})
 public class ApiDemoApplication extends SpringBootServletInitializer{
 
