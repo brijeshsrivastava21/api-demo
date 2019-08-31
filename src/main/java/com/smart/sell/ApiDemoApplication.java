1858ac762094b076b0cf1d95108574dd3ec4a55f
Srivastava <brijesh.c.kumar@capgemini.com>
2019-08-31 13:19:27 +0530

    Chnages.

--- src/main/java/com/smart/sell/ApiDemoApplication.java
+++ src/main/java/com/smart/sell/ApiDemoApplication.java
@@ -6,6 +6,7 @@ package com.smart.sell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -16,5 +17,10 @@ public class ApiDemoApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(ApiDemoApplication.class, args);
 	}
+	
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(ApiDemoApplication.class);
+	  }
 
 }
